0aed4ee99d75ef2c7ad2499a9ed648a36b5ba642
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-25 17:13:35 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#index'
+
+   get '/login', to: 'sessions#new'
+   post '/login', to: 'sessions#create'
+   post '/logout', to: 'sessions#destroy'
+
+   get '/secret', to: 'secrets#show'
 end
